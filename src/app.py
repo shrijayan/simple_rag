602989776a11bd514338740861c2dc3f7187367f
shrijayan <shrijayan.rajendran@thoughtworks.com>
2024-08-04 20:00:06 +0530

    main file modification

--- src/app.py
+++ src/app.py
@@ -1,6 +1,5 @@
 import os
 import streamlit as st
-import ollama
 import chromadb
 from openai import OpenAI
 from pathlib import Path
@@ -11,15 +10,6 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 # Initialize ChromaDB client
 chroma_client = chromadb.PersistentClient(path="data/vdb")
 collection = chroma_client.get_or_create_collection(name="chromadb_collection_name")
-
-def llm(user_message, model_name):
-    response = ollama.chat(
-        model=model_name,
-        messages=[{'role': 'user', 'content': user_message}],
-    )
-    print(response['message']['content'])   
-    return response['message']['content']
-
 
 # Sidebar layout
 with st.sidebar:
@@ -56,34 +46,16 @@ if selected_page == 'Chat':
             st.write("Please enter a question.")
             
 elif selected_page == 'Database':
-    # Function to create directories if they do not exist
-    # def create_directories():
-    #     if not os.path.exists('data'):
-    #         os.mkdir('data')
-    #     if not os.path.exists('data/raw_pdf'):
-    #         os.mkdir('data/raw_pdf')
-    #         os.mkdir('data/processed_data')
-
-    # Function to save uploaded file
     def save_uploaded_file(uploaded_file):
         # Set the folder where uploaded files will be saved
         upload_folder = 'data/raw_pdf'
         os.makedirs(upload_folder, exist_ok=True)
-        # if uploaded_files is not None:
-        #     for uploaded_file in uploaded_files:
-        #         # Save each uploaded file
-        #         file_path = os.path.join('data/raw_data', uploaded_file.name)
-        #         with open(file_path, "wb") as f:
-        #             f.write(uploaded_file.getbuffer())
-        #         st.success(f"File {uploaded_file.name} saved successfully!")
-
         if uploaded_files:
             for uploaded_file in uploaded_files:
                 # Save the uploaded file to the specified folder
                 with open(os.path.join(upload_folder, uploaded_file.name), 'wb') as f:
                     f.write(uploaded_file.getbuffer())
                 st.success(f"Saved file: {uploaded_file.name}")
-
         st.write("Upload files to save them to the 'data/raw_pdf' folder.") 
 
     # Function to extract text from PDF
@@ -123,21 +95,9 @@ elif selected_page == 'Database':
         collection.add(
         documents = vdb_data,
         ids = [str(i) for i in range(len(vdb_data))])
-    
-    def use_selected_collections_for_rag(selected_collections):
-        # Placeholder function to demonstrate using selected collections
-        st.write("Using the following collections for RAG chatting:")
-        for collection in selected_collections:
-            st.write(collection)
-
-    # Create directories
-    # create_directories()
 
     # Streamlit app
     st.title("Database")
-
-    # Create directories
-    # create_directories()
 
     # Initialize ChromaDB client
     chroma_client = initialize_vector_db()
@@ -168,22 +128,8 @@ elif selected_page == 'Database':
     st.subheader("Create New Collection")
     collection_name = st.text_input("Enter collection name")
     uploaded_files = st.file_uploader("Upload files", type=["pdf"], accept_multiple_files=True)
-    
     if st.button("Create Collection") and collection_name and uploaded_files:
         save_uploaded_file(uploaded_files)
         extract_text_from_pdf()
-        # Save uploaded files
         file_paths = add_document_to_vector_db()
-        st.success(f"Files uploaded and saved to 'data/raw_pdf'!")
-
-        # # Extract and combine text from all PDFs
-        # combined_text = ""
-        # for file_path in file_paths:
-        #     combined_text += extract_text_from_pdf(file_path)
-
-        # Create text chunks
-        # text_chunks = text_splitter(combined_text)
-
-        # Add document text to vector database
-        # add_document_to_vector_db(collection, text_chunks)
         st.success(f"Text from uploaded files added to collection '{collection_name}' in vector database!")
